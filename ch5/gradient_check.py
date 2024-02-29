cd142c938a912652deca8c236f189ed0755d967b
jaewoogwak <jaewoo010207@gmail.com>
2024-02-29 17:27:12 +0900

    add: 5장

---
+++ ch5/gradient_check.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+path = os.getcwd()
+parent = os.path.join(path, os.pardir)
+sys.path.append(os.path.abspath(parent))
+from dataset.mnist import load_mnist
+from TwoLayerNet import TwoLayerNet
+import numpy as np
+
+# 데이터 읽기
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+x_batch = x_train[:3]
+t_batch = t_train[:3]
+
+grad_numerical = network.numerical_gradient(x_batch, t_batch)
+grad_backprop = network.gradient(x_batch, t_batch)
+
+# 각 가중치의 차이의 절댓값을 구한 후, 그 절댓값들의 평균을 낸다
+for key in grad_numerical.keys():
+    diff = np.average(np.abs(grad_backprop[key] - grad_numerical[key]))
+    print(key + ":" + str(diff))
--- common/layers.py
+++ common/layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from common.functions import softmax, cross_entropy_error
 
 
 class Relu:
@@ -31,5 +32,46 @@ class Sigmoid:
 
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None  # 손실값
+        self.y = None  # softmax의 출력값
+        self.t = None  # 정답 레이블(원-핫 벡터)
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
 
         return dx
